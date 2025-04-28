424eb96dd8bc303d15e5fed605ddfced52c77dae
nailahusein <naile3690@gmail.com>
2025-04-28 01:00:44 -0700

    Changes pushed

--- src/test/java/com/google_finance/runner/CukesRunner.java
+++ src/test/java/com/google_finance/runner/CukesRunner.java
@@ -2,20 +2,23 @@ package com.google_finance.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
+        features = "src/test/resources/*.feature",
+        glue = "com.google_finance.steps",
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
-        features = "src/test/resources",
-        glue = "com/google_finance/steps",
-        dryRun = true,
-        tags = "",
         publish = true
 )
 public class CukesRunner extends AbstractTestNGCucumberTests {
 
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
-
